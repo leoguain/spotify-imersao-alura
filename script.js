783d0e9459954fa86bd3fb50ebd3fdbb2e962925
leoguain <108464746+leoguain@users.noreply.github.com>
2024-01-25 16:41:20 -0300

    Update Aula 4

---
+++ script.js
@@ -0,0 +1,101 @@
+const searchInput = document.getElementById("search-input");
+const resultArtists = document.getElementById("result-artists");
+const resultPlaylists = document.getElementById("result-playlists");
+
+function loadPlaylists(searchTerm) {
+  const urlApi = searchTerm
+    ? `http://localhost:3000/playlists?genre_like${searchTerm}`
+    : `http://localhost:3000/playlists`;
+
+  fetch(urlApi)
+    .then((response) => response.json())
+    .then((results) => displayPlaylists(results));
+}
+
+function displayPlaylists(results) {
+  resultPlaylists.innerHTML = "";
+  resultPlaylists.classList.remove("hidden");
+
+  results.forEach((element) => {
+    var itemContainer = document.createElement("div");
+    var itemThumb = document.createElement("img");
+    var itemTitle = document.createElement("span");
+    var itemDescription = document.createElement("span");
+    var playIcon = document.createElement("span");
+
+    var description = element.description;
+
+    if (description.length > 40) {
+      description = description.substring(0, 40) + "...";
+    }
+
+    itemContainer.classList.add("playlist__card");
+    itemThumb.classList.add("playlist__img");
+    itemTitle.classList.add("playlist__title");
+    itemDescription.classList.add("playlist__description");
+    playIcon.classList.add("fa");
+    playIcon.classList.add("fa-play");
+
+    itemThumb.src = element.urlImg;
+    itemTitle.textContent = element.name;
+    itemDescription.textContent = description;
+
+    itemContainer.appendChild(itemThumb);
+    itemContainer.appendChild(playIcon);
+    itemContainer.appendChild(itemTitle);
+    itemContainer.appendChild(itemDescription);
+
+    resultPlaylists.appendChild(itemContainer);
+  });
+}
+
+function requestApi(searchTerm) {
+  fetch(`http://localhost:3000/artists`)
+    .then((response) => response.json())
+    .then((results) => displayArtists(results));
+}
+
+function displayArtists(results) {
+  resultArtists.innerHTML = "";
+  resultArtists.classList.remove("hidden");
+
+  results.forEach((element) => {
+    var itemContainer = document.createElement("div");
+    var itemThumb = document.createElement("img");
+    var itemTitle = document.createElement("span");
+    var itemDescription = document.createElement("span");
+    var playIcon = document.createElement("span");
+
+    itemContainer.classList.add("playlist__card");
+    itemThumb.classList.add("artist__img");
+    itemTitle.classList.add("playlist__title");
+    itemDescription.classList.add("playlist__description");
+    playIcon.classList.add("fa");
+    playIcon.classList.add("fa-play");
+
+    itemThumb.src = element.urlImg;
+    itemTitle.textContent = element.name;
+    itemDescription.textContent = "Artista";
+
+    itemContainer.appendChild(itemThumb);
+    itemContainer.appendChild(playIcon);
+    itemContainer.appendChild(itemTitle);
+    itemContainer.appendChild(itemDescription);
+    resultArtists.appendChild(itemContainer);
+  });
+}
+
+document.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  if (searchTerm === "") {
+    resultPlaylists.classList.remove("hidden");
+    resultArtists.classList.add("hidden");
+    return;
+  }
+
+  requestApi(searchTerm);
+  loadPlaylists(searchTerm);
+});
+
+loadPlaylists();
